3395b8e9cddfd86b2ea5a264cf1be55b0ef2978a
raulzin <79669644+raulzin@users.noreply.github.com>
2025-03-20 19:54:30 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,31 +1,31 @@
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Produto {
     private String nome;
     private double preco;
     private int quantidade;
-}
 
+    // Construtor correto
     public Produto(String nome, int quantidade, double preco) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.preco = preco;
-}
+    }
 
-public String getNome(){
-    return nome;
-}
+    public String getNome() {
+        return nome;
+    }
 
-public double getPreco(){
-    return preco;
-}
+    public double getPreco() {
+        return preco;
+    }
 
-public int getQuantidade(){
-    return quantidade;
-}
+    public int getQuantidade() {
+        return quantidade;
+    }
 
-public void setQuantidade(int quantidade){
-    this.quantidade = quantidade;
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
