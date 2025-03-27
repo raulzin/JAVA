db0abcbdfd8dcd2cda3c954190fceb12395f9bfd
raulzin <raulsanttos293@gmail.com>
2025-03-27 19:34:58 -0300

    Alteração na HomeScreen e correção de bugs

--- TaskDavi/Pessoa.java
+++ TaskDavi/Pessoa.java
@@ -1,9 +1,5 @@
 // Removed package declaration
 public class Pessoa {
-    public static void main(String[] args) {
-        Pessoa p = new Pessoa(); 
-        p.dizerOla(); 
-    }
     public void dizerOla() {
         System.out.println("Olá, mundo!");
     }
